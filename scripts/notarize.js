06b5852c96c7023c05224e01a0db111094e70fa0
MuthubalasudhanMS <muthubala.sudhanmahalingam@iamneo.ai>
2025-06-17 14:53:28 +0530

    notarize modified

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,18 +1,24 @@
 require('dotenv').config();
 const { notarize } = require('@electron/notarize');
 
-exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;
-
-  if (electronPlatformName !== 'darwin') return;
-
+exports.default = async function notarizeApp(context) {
+  const { appOutDir } = context;
   const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
-    appBundleId: 'com.example.localupdate',
+  if (!process.env.APPLE_ID || !process.env.APPLE_ID_PASS || !process.env.ASC_PROVIDER) {
+    console.warn("Missing Apple notarization credentials. Skipping notarization.");
+    return;
+  }
+
+  console.log(`Notarizing ${appName}.app`);
+
+  await notarize({
+    appBundleId: 'com.example.app', // replace with your app's bundle ID
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_ID_PASS,
     ascProvider: process.env.ASC_PROVIDER,
   });
+
+  console.log(`Done notarizing ${appName}.app`);
 };
